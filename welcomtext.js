4c566551acd8f1f6df82e2aca49339ff2e617d71
mayconSKYT <81333833+mayconSKYT@users.noreply.github.com>
2021-05-01 12:39:36 -0300

    Update welcomtext.js

--- welcomtext.js
+++ welcomtext.js
@@ -1,9 +1,9 @@
 const welcome = (number, groupname) => {
-    return `𝐎𝐥𝐚 @${number}. 𝐒𝐞𝐣𝐚 𝐛𝐞𝐦-𝐯𝐢𝐧𝐝𝐨 𝐚𝐨 𝐠𝐫𝐮𝐩𝐨 ${groupname}`
+    return `𝐎𝐥𝐚 @${number}. 𝐒𝐞𝐣𝐚 𝐛𝐞𝐦-𝐯𝐢𝐧𝐝𝐨 𝐚𝐨 𝐠𝐫𝐮𝐩𝐨 💢🔞ƶσε૨α รεɱ ℓเɱเƭεร🔞💢 ${groupname}`
 }
 exports.welcome = welcome
 
 const bye = (number) => {
-    return `Adeus @${number}. Já vai tarde 👋`
+    return `Adeus @${number}. VAZA LIXO 👋`
 }
 exports.bye = bye
